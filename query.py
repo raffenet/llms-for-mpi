9041a37fa8e3a8988332432339d37265022f8cc3
Edwin Mizen <edwinmizen2026@u.northwestern.edu>
2024-06-28 13:25:27 -0500

    Trying stuff out

--- query.py
+++ query.py
@@ -1,2 +1,8 @@
-print('hello')
-exit(0)
+with open('prompts.txt', 'r') as prompts:
+    lines = prompts.readlines()
+    for line in lines:
+        
+    prompts.close()
+
+#print('hello')
+#exit(0)
