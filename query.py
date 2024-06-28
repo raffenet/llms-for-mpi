312c7b50cc72b7b6fe956f8460b5d43afe555faf
Edwin Mizen <edwinmizen2026@u.northwestern.edu>
2024-06-28 13:25:46 -0500

    Hi

--- query.py
+++ query.py
@@ -1,8 +1,9 @@
+#open the prompts, read each line of the prompts, and feed it to each of the models 
 with open('prompts.txt', 'r') as prompts:
     lines = prompts.readlines()
     for line in lines:
-        
-    prompts.close()
 
-#print('hello')
-#exit(0)
+#    prompts.close()
+
+print('hello')
+exit(0)
