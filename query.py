6e5ad1844f34d25dcba6f33c4fcb8cbf7a5adbb0
Edwin Mizen <edwinmizen2026@u.northwestern.edu>
2024-08-09 10:53:24 -0500

    commit these

--- query.py
+++ query.py
@@ -1,9 +1,35 @@
+import os
+import sys
+sys.path.append(os.path.abspath("/scratch/emizen/falcon7b/falcon"))
+
+# next step, add in llama stuff:
+# check if I need to add path to llama stuff
+# import llama stuff
+# modify llama model so that it's a function also
+# feed same prompts to llama and then 
+
+# import all of my falcon stuff 
+import falcon
+print(dir(falcon))
+
+
+# import all of my llama stuff
+#import llama
+#print(dir(llama))
+
 #open the prompts, read each line of the prompts, and feed it to each of the models 
 with open('prompts.txt', 'r') as prompts:
-    lines = prompts.readlines()
-    for line in lines:
+    lines = prompts.read().splitlines()
+    print(lines)
+    prompts.close()
 
-#    prompts.close()
-
-print('hello')
-exit(0)
+#open the example directory/iterate through examples
+for filename in os.listdir('examples'):
+    with open('examples/{}'.format(filename), 'r') as e:
+        code = e.read()
+        #add prompt to example
+        for line in lines:
+            falcon.run_falcon(line + code)
+            #llama.run_llama(line + code)
+        e.close()  
+        
